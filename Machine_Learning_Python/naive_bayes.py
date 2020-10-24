e6a5b904dbfcfd1d016ea26573eb4b18c7c1a7ed
MarcosBezerra <mlcbezerra@hotmail.com>
2020-10-24 13:48:34 -0300

    técnicas machine learning

--- Machine_Learning_Python/naive_bayes.py
+++ Machine_Learning_Python/naive_bayes.py
@@ -1,13 +1,14 @@
-# Este exemplo carrega a base Wine da UCI, e avalia dois KNNs
-# Um usando holdout e outro usando validaÃ§Ã£o cruzada com 10 pastas. 
+# Este exemplo carrega a base Wine da UCI, treina um classificador Naive Bayes
+# usando holdout e outro usando validaÃ§Ã£o cruzada com 10 pastas. 
 
-# Importa bibliotecas necessárias 
+# Importa bibliotecas necessÃ¡rias 
 import numpy as np
 import urllib
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn import  model_selection
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
+
 #from sklearn.model_selection import StratifiedShuffleSplit
 # Carrega uma base de dados do UCI
 # Exemplo carrega a base Wine
@@ -17,7 +18,7 @@ raw_data = urllib.request.urlopen(url)
 # Carrega arquivo como uma matriz
 dataset = np.loadtxt(raw_data, delimiter=",")
 
-# Imprime quantide de instâncias e atributos da base
+# Imprime quantide de instÃ¢ncias e atributos da base
 print(dataset.shape)
 
 # Coloca em X os 13 atributos de entrada e em y as classes
@@ -29,10 +30,8 @@ y = dataset[:,0]
 # Holdout -> dividindo a base em treinamento (70%) e teste (30%), estratificada
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=.3, random_state=42, stratify=y)
 
-# declara o classificador
-clfa = KNeighborsClassifier(n_neighbors=3)
-
-# treina o classificador
+# Treina o classificador
+clfa = GaussianNB()
 clfa = clfa.fit(X_train, y_train)
 
 # testa usando a base de testes
@@ -49,16 +48,17 @@ print("Accuracy = %.2f " % score)
 print("Confusion Matrix:")
 print(matrix)
 
-# EXEMPLO USANDO VALIDAÇÂO CRUZADA
-clfb = KNeighborsClassifier(n_neighbors=3)
+# EXEMPLO USANDO VALIDAÃ‡ÃƒO CRUZADA
+clfb = GaussianNB()
 folds=10
 result = model_selection.cross_val_score(clfb, X, y, cv=folds)
 print("\nCross Validation Results %d folds:" % folds)
 print("Mean Accuracy: %.2f" % result.mean())
 print("Mean Std: %.2f" % result.std())
 
-# matriz de confusÃ£o da validação cruzada
+# matriz de confusÃ£o da validaÃ§Ã£o cruzada
 Z = model_selection.cross_val_predict(clfb, X, y, cv=folds)
 cm=confusion_matrix(y, Z)
 print("Confusion Matrix:")
 print(cm)
+
